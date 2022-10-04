cc4510c85f99ce5e95117dcd9994f9a1baf38d86
rnwatt <55397739+rnwatt@users.noreply.github.com>
2022-10-04 16:37:38 +0100

    Home page redirect login
    After successful login, redirects to the main page

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ button.addEventListener('click', async _ => {
             method: 'post',
             body: JSON.stringify(loginBody)
         });
+        let emailAdd = document.getElementById("email").value;
+        let passwordAdd = document.getElementById("password").value;
+        //test values
+        if (emailAdd === "test@test.com" && passwordAdd === "testPassword") {
+            window.location.replace("index.html");
+        } else {
+            alert("Please make sure your username and password is entered correctly.");
+            return;
+        }
         console.log('Completed!', response.status);
     } catch (err) {
         console.error(`Error: ${err}`);
