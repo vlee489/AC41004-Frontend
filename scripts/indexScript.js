377b89e7774a3ae13f4d5e85646e5cb67ca57953
Hope <hopehill@me.com>
2022-10-05 15:15:25 +0100

    load rules table from fixed ID

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,6 +1,5 @@
-const button2 = document.getElementById('navbarDropdown');
-
-button2.addEventListener('click', async _ => {
+// Loads accounts user has access to into Accounts Dropdown in Nav Bar
+async function loadAccounts(){
   try {     
     const response = await fetch('https://itp.vlee.me.uk/account/access', {
       headers: {"Content-type": "application/json"},
@@ -8,16 +7,15 @@ button2.addEventListener('click', async _ => {
       credentials:"include"
     }).then(response => response.json())
     .then(json => {
-  
         // Create a variable to store HTML
         let li = ``;
+        // href = "/${user.id}"
         
-       
         // Loop through each data and add a table row
         json.forEach(user => {
             li += `<li><a class="dropdown-item" href="#">${user.name} (${user.reference})</a></li>`;
         });
-  
+    
     // Display result
     document.getElementById("accountList").innerHTML = li;
 });
@@ -25,4 +23,51 @@ button2.addEventListener('click', async _ => {
     console.error(`Error: ${err}`);
   }
   
+};
+
+loadAccounts();
+
+// Sets the account to the one selected in the Accounts Dropdown in Nav Bar
+const button3 = document.getElementById('accountListItem');
+
+button3.addEventListener('click', async _ => {
+try{
+    console.log('clicked');
+}catch(err) {
+    console.error(`Error: ${err}`);
+  }
+
 });
+
+// Fetches compliance rules and the number of non-compliant resources into the Compliance Rules tables
+async function loadRules(){
+    try {     
+      const response = await fetch('https://itp.vlee.me.uk/ruleOverview/633ad7aca938b45d958ae772', {
+        headers: {"Content-type": "application/json"},
+        method: 'get',
+        credentials:"include"
+      }).then(response => response.json())
+      .then(json => {
+          // Create a variable to store HTML
+          let li = ``;
+          // Loop through each data and add a table row
+          json.forEach(user => {
+            const name = user['rule']['name'];
+            const length = user['non_compliant'].length;
+            if (length >0) {
+                li += `<tr>
+                <td>${name}</td>
+                <td >${length}</td>
+                <td ><button type="button" class="btn btn-warning"><i class="fa-regular fa-pen-to-square"></i></button></td>
+              </tr>`
+            }
+        });
+      // Display result
+      document.getElementById("rule-table").innerHTML = li;
+  });
+    } catch(err) {
+      console.error(`Error: ${err}`);
+    }
+}
+
+loadRules();
