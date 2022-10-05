63f8a526eb6558c215dbae4a21565cc38a234807
Hope <hopehill@me.com>
2022-10-05 16:11:37 +0100

    automatically selects last account id to load

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,3 +1,5 @@
+let accountID = "";
+
 // Loads accounts user has access to into Accounts Dropdown in Nav Bar
 async function loadAccounts(){
   try {     
@@ -9,15 +11,22 @@ async function loadAccounts(){
     .then(json => {
         // Create a variable to store HTML
         let li = ``;
+        let dd = ``;
         // href = "/${user.id}"
         
         // Loop through each data and add a table row
         json.forEach(user => {
             li += `<li><a class="dropdown-item" href="#">${user.name} (${user.reference})</a></li>`;
+            dd += `<option selected value=${user.id}>${user.name}</option>`
+            accountID = user.id;
+           
+
         });
     
     // Display result
     document.getElementById("accountList").innerHTML = li;
+    document.getElementById("accountSelect").innerHTML = dd;
+    
 });
   } catch(err) {
     console.error(`Error: ${err}`);
@@ -25,7 +34,7 @@ async function loadAccounts(){
   
 };
 
-loadAccounts();
+//loadAccounts();
 
 // Sets the account to the one selected in the Accounts Dropdown in Nav Bar
 const button3 = document.getElementById('accountListItem');
@@ -41,8 +50,11 @@ try{
 
 // Fetches compliance rules and the number of non-compliant resources into the Compliance Rules tables
 async function loadRules(){
+    
     try {     
-      const response = await fetch('https://itp.vlee.me.uk/ruleOverview/633ad7aca938b45d958ae772', {
+        //633ad7aca938b45d958ae772
+        await loadAccounts();
+      const response = await fetch(`https://itp.vlee.me.uk/ruleOverview/${accountID}`, {
         headers: {"Content-type": "application/json"},
         method: 'get',
         credentials:"include"
